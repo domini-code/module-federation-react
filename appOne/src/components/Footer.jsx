710275929bad2e77b1e725d128efafc2388083ed
bezael <bezael@gmail.com>
2023-04-08 19:24:46 +0200

    remove old apps

--- appOne/src/components/Footer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Footer = () => {
-  return <div className='p-5 bg-blue-600 text-white text-3xl'>FOOTER</div>;
-};
-
-export default Footer;
--- appOne/src/components/HomeContent.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-const Home = () => {
-  return <div className='grid grid-cols-4 gap-5 h-96'>Home content</div>;
-};
-
-export default Home;
